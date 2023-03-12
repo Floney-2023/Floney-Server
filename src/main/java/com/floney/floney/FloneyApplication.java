061052772967dc74d3aab43134798772b4e60ab9
psyeon1120 <psycsh@naver.com>
2023-03-12 17:45:01 +0900

    first commit

---
+++ src/main/java/com/floney/floney/FloneyApplication.java
@@ -0,0 +1,13 @@
+package com.floney.floney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/floney/floney/FloneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floney.floney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
