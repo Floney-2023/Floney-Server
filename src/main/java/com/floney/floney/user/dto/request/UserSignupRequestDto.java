d78e11cb84503c87f32177e5b73f7f43fa6200a9
sen <seeunchoi99@gmail.com>
2023-04-09 16:43:14 +0900

    #3 feat: User entity 수정 및 그에 따른 dto 수정

---
+++ src/main/java/com/floney/floney/user/dto/request/UserSignupRequestDto.java
@@ -0,0 +1,27 @@
+package com.floney.floney.user.dto.request;
+
+import com.floney.floney.user.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserSignupRequestDto {
+    private final String email;
+    private final String nickname;
+    private final String password;
+    private final int marketingAgree;
+
+    public UserDto to() {
+        return UserDto.of(
+                nickname,
+                email,
+                password,
+                null,
+                marketingAgree,
+                0,
+                null,
+                "email"
+        );
+    }
+}
