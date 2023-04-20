e5af9c742d68cdc29c68a84dda0f937e93dfc8b2
sienna <20200803@sungshin.ac.kr>
2023-04-20 12:54:58 +0900

    test : 가계부 코드와 유저 이메일로 bookUser를 조회한다

--- src/main/java/com/floney/floney/book/repository/BookUserCustomRepository.java
+++ src/main/java/com/floney/floney/book/repository/BookUserCustomRepository.java
@@ -1,8 +1,12 @@
 package com.floney.floney.book.repository;
 
+import com.floney.floney.book.dto.CreateLineRequest;
 import com.floney.floney.book.entity.Book;
+import com.floney.floney.book.entity.BookUser;
 
 public interface BookUserCustomRepository {
 
     void isMax(Book book);
+
+    BookUser findUserWith(String auth, String bookKey);
 }
--- src/main/java/com/floney/floney/book/repository/BookUserRepositoryImpl.java
+++ src/main/java/com/floney/floney/book/repository/BookUserRepositoryImpl.java
@@ -1,12 +1,17 @@
 package com.floney.floney.book.repository;
 
 import com.floney.floney.book.entity.Book;
+import com.floney.floney.book.entity.BookUser;
 import com.floney.floney.common.exception.MaxMemberException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
+import static com.floney.floney.book.entity.QBook.book;
 import static com.floney.floney.book.entity.QBookUser.bookUser;
+import static com.floney.floney.user.entity.QUser.user;
 
 @Repository
 @RequiredArgsConstructor
@@ -26,5 +31,17 @@ public class BookUserRepositoryImpl implements BookUserCustomRepository {
         if (memberCount > MAX_MEMBER) {
             throw new MaxMemberException();
         }
+    }
+
+    @Override
+    public BookUser findUserWith(String auth, String bookKey){
+        return jpaQueryFactory
+            .select(bookUser)
+            .from(bookUser)
+            .innerJoin(bookUser.user,user)
+            .where(user.email.eq(auth))
+            .innerJoin(bookUser.book,book)
+            .where(book.bookKey.eq(UUID.fromString(bookKey)))
+            .fetchOne();
     }
 }
