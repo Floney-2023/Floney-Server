355aca9ca6a39e3bf540ea131516f8175ca05182
sen <seeunchoi99@gmail.com>
2023-04-30 11:09:17 +0900

    #3 chore: findByEmail 반환값 Optional로 수정

--- src/main/java/com/floney/floney/user/repository/UserRepository.java
+++ src/main/java/com/floney/floney/user/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.floney.floney.user.repository;
 
 import com.floney.floney.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
