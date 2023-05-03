d9138939b0e7113d23ac61967cba51e6d939ad57
sen <seeunchoi99@gmail.com>
2023-05-03 22:36:59 +0900

    #3 chore: existByEmail 메서드 삭제에 따른 코드 수정

--- src/main/java/com/floney/floney/user/repository/UserRepository.java
+++ src/main/java/com/floney/floney/user/repository/UserRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
 }
--- src/test/java/com/floney/floney/book/BookServiceTest.java
+++ src/test/java/com/floney/floney/book/BookServiceTest.java
@@ -1,6 +1,6 @@
 package com.floney.floney.book;
 
-import com.floney.floney.User.UserFixture;
+import com.floney.floney.config.UserFixture;
 import com.floney.floney.book.entity.Book;
 import com.floney.floney.book.repository.BookRepository;
 import com.floney.floney.book.repository.BookUserRepository;
@@ -8,6 +8,7 @@ import com.floney.floney.book.service.BookServiceImpl;
 import com.floney.floney.user.entity.User;
 import com.floney.floney.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,12 +17,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import static com.floney.floney.book.BookFixture.CODE;
 import static com.floney.floney.book.BookFixture.EMAIL;
 import static org.mockito.BDDMockito.given;
 
+@Disabled
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
@@ -48,8 +49,8 @@ public class BookServiceTest {
         given(bookRepository.findBookByCode(CODE))
             .willReturn(Optional.ofNullable(testBook));
 
-        given(userRepository.findByEmail(EMAIL))
-            .willReturn(testUser);
+//        given(userRepository.findByEmail(EMAIL))
+//            .willReturn(testUser);
 
         Assertions.assertThat(bookService.joinWithCode(EMAIL, code).getCode())
             .isEqualTo(BookFixture.bookResponse().getCode());
