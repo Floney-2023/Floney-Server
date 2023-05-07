3dd9a778e035f25bc3effa7a390eb3071179c3a3
sienna <20200803@sungshin.ac.kr>
2023-05-07 17:58:47 +0900

    #14 fix : userDto 에러 수정

--- src/main/java/com/floney/floney/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/floney/floney/user/dto/request/UserLoginRequest.java
@@ -2,9 +2,10 @@ package com.floney.floney.user.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserLoginRequest {
     private String email;
     private String password;
