dd3151cd261ee721fef35ebde0c7d706b1cf9ca6
sienna <20200803@sungshin.ac.kr>
2023-05-07 18:37:52 +0900

    #14 refactor : 자산 범위 검사 로직 삭제

--- src/main/java/com/floney/floney/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/floney/floney/user/dto/request/UserLoginRequest.java
@@ -9,4 +9,9 @@ import lombok.RequiredArgsConstructor;
 public class UserLoginRequest {
     private String email;
     private String password;
+
+    public UserLoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
