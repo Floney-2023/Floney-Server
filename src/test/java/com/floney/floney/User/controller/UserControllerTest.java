eb6e8eca5fac1b4060e52d62854943e3af525aeb
sen <seeunchoi99@gmail.com>
2023-05-15 23:51:18 +0900

    #16 test: userControllerTest 추가
    로그인, 이메일 전송 관련 테스트 추가

--- src/test/java/com/floney/floney/User/controller/UserControllerTest.java
+++ src/test/java/com/floney/floney/User/controller/UserControllerTest.java
@@ -1,34 +1,37 @@
 package com.floney.floney.User.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.floney.floney.config.UserFixture;
+import com.floney.floney.common.exception.MailAddressException;
+import com.floney.floney.common.token.dto.Token;
 import com.floney.floney.user.dto.request.LoginRequest;
-import com.floney.floney.user.entity.User;
-import org.junit.jupiter.api.Disabled;
+import com.floney.floney.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.mail.MailSendException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@Disabled
 class UserControllerTest {
 
     private final MockMvc mockMvc;
     private final ObjectMapper objectMapper;
+    @MockBean private UserService userService;
 
-    // TODO: [java.lang.OutOfMemoryError: Java heap space] 로 인한 테스트 실행 실패
     @Autowired
     public UserControllerTest(MockMvc mockMvc, ObjectMapper objectMapper) {
         this.mockMvc = mockMvc;
@@ -37,20 +40,76 @@ class UserControllerTest {
 
     @Test
     @DisplayName("로그인에 성공한다")
-    @WithUserDetails(userDetailsServiceBeanName = "testUserDetailService", setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void login() throws Exception {
+    void login_success() throws Exception {
         // given
-        User user = UserFixture.getUser();
         LoginRequest requestUser = LoginRequest.builder()
-                .email(user.getEmail())
-                .password(user.getPassword())
+                .email("success@email.com")
+                .password("success")
                 .build();
+        given(userService.login(any(LoginRequest.class))).willReturn(new Token("", ""));
 
         // when & then
         mockMvc.perform(post("/users/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestUser)))
                 .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @DisplayName("로그인에 실패한다 - 잘못된 아이디 혹은 비밀번호")
+    void login_fail_invalidInput() throws Exception {
+        // given
+        LoginRequest requestUser = LoginRequest.builder()
+                .email("wrong@email.com")
+                .password("wrong")
+                .build();
+        given(userService.login(any(LoginRequest.class))).willThrow(BadCredentialsException.class);
+
+        // when & then
+        mockMvc.perform(post("/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestUser)))
+                .andExpect(status().is4xxClientError());
+        then(userService).should().login(requestUser);
+    }
+
+    @Test
+    @DisplayName("인증용 이메일을 보내는 데 성공한다")
+    void sendAuthenticateEmail_success() throws Exception {
+        // given
+        String email = "right@email.com";
+        given(userService.sendAuthenticateEmail(email)).willReturn(anyString());
+
+        // when & then
+        mockMvc.perform(get("/users/email").queryParam("email", email))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN));
+    }
+
+    @Test
+    @DisplayName("인증용 이메일을 보내는 데 실패한다 - 잘못된 이메일 형식")
+    void sendAuthenticateEmail_fail_invalidEmail() throws Exception {
+        // given
+        String email = "wrong@email.com";
+        given(userService.sendAuthenticateEmail(email)).willThrow(new MailAddressException());
+
+        // when & then
+        mockMvc.perform(get("/users/email").queryParam("email", email))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @DisplayName("인증용 이메일을 보내는 데 실패한다 - 메일 서버 오류")
+    void sendAuthenticateEmail_fail_mailServerError() throws Exception {
+        // given
+        String email = "right@email.com";
+        given(userService.sendAuthenticateEmail(email)).willThrow(MailSendException.class);
+
+        // when & then
+        mockMvc.perform(get("/users/email").queryParam("email", email))
+                .andExpect(status().isInternalServerError())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 }
