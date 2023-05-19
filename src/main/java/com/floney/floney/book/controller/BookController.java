0a906d23e226f60ec77d4876388b84aa0a2dbed1
sienna <20200803@sungshin.ac.kr>
2023-05-19 11:18:31 +0900

    #36 refactor : 불필요한 import 삭제

--- src/main/java/com/floney/floney/book/controller/BookController.java
+++ src/main/java/com/floney/floney/book/controller/BookController.java
@@ -9,9 +9,7 @@ import com.floney.floney.book.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
