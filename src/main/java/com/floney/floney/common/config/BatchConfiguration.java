e209e6905b6333249d03a89b8818b1514acd063d
sienna <20200803@sungshin.ac.kr>
2023-07-02 00:33:01 +0900

    #81 fix : Batch 설정 분리

---
+++ src/main/java/com/floney/floney/common/config/BatchConfiguration.java
@@ -0,0 +1,10 @@
+package com.floney.floney.common.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+}
