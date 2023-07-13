78002e24eaed073025d18f7b2f3ea2f120a7a7cd
sienna <20200803@sungshin.ac.kr>
2023-07-13 20:32:08 +0900

    #85 fix : 괄호 추가

--- src/main/java/com/floney/floney/book/controller/BookController.java
+++ src/main/java/com/floney/floney/book/controller/BookController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
