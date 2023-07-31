f9aca174ebb5b43c118c9ce859b1c0022976ce06
sen <seeunchoi99@gmail.com>
2023-07-31 18:56:58 +0900

    #112 chore: SettlementController 메서드에 주석 추가

--- src/main/java/com/floney/floney/settlement/controller/SettlementController.java
+++ src/main/java/com/floney/floney/settlement/controller/SettlementController.java
@@ -23,17 +23,33 @@ public class SettlementController {
 
     private final SettlementService settlementService;
 
+    /**
+     * 정산 내역 생성
+     * @param request 정산 내역 생성 요청
+     * @param userDetails 요청한 유저 정보
+     * @return 생성한 정산 내역 응답
+     */
     @PostMapping("")
     public ResponseEntity<?> createSettlement(@RequestBody @Valid SettlementRequest request,
                                               @AuthenticationPrincipal CustomUserDetails userDetails) {
         return new ResponseEntity<>(settlementService.create(request, userDetails), HttpStatus.CREATED);
     }
 
+    /**
+     * 가계부의 모든 정산 내역 조회
+     * @param bookKey 가계부 key
+     * @return 정산 내역 리스트
+     */
     @GetMapping("")
     public ResponseEntity<?> getSettlementsByBook(@RequestParam String bookKey) {
         return new ResponseEntity<>(settlementService.findAll(bookKey), HttpStatus.OK);
     }
 
+    /**
+     * 정산 내역 조회
+     * @param id 정산 내역 id(pk)
+     * @return 요청한 정산 내역 응답
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getSettlement(@PathVariable Long id) {
         return new ResponseEntity<>(settlementService.find(id), HttpStatus.OK);
