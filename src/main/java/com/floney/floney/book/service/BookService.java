37c67ad8cb5fac0b3d03264279d0eb71c047f78a
sienna <20200803@sungshin.ac.kr>
2023-07-31 20:36:31 +0900

    #113 feat : 카테고리별 분석 기능 구현

--- src/main/java/com/floney/floney/book/service/BookService.java
+++ src/main/java/com/floney/floney/book/service/BookService.java
@@ -1,17 +1,15 @@
 package com.floney.floney.book.service;
 
+import com.floney.floney.book.dto.process.AnalyzeResponse;
+import com.floney.floney.book.dto.process.OurBookInfo;
 import com.floney.floney.book.dto.request.*;
 import com.floney.floney.book.dto.response.CheckBookValidResponse;
 import com.floney.floney.book.dto.response.CreateBookResponse;
-import com.floney.floney.book.dto.process.OurBookInfo;
-import com.floney.floney.book.dto.request.SeeProfileRequest;
-import com.floney.floney.book.dto.request.UpdateAssetRequest;
-import com.floney.floney.book.dto.request.UpdateBookImgRequest;
-import com.floney.floney.book.dto.request.UpdateBudgetRequest;
 import com.floney.floney.book.dto.response.InviteCodeResponse;
 import com.floney.floney.book.entity.Book;
 import com.floney.floney.user.dto.response.UserResponse;
 import com.floney.floney.user.dto.security.CustomUserDetails;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -48,4 +46,6 @@ public interface BookService {
     void bookUserOut(BookUserOutRequest request, String username);
 
     InviteCodeResponse inviteCode(String bookKey);
+
+    AnalyzeResponse analyzeByCategory(AnalyzeByCategoryRequest request);
 }
