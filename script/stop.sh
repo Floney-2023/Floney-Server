adc9042ca8cb85906fea7bd110e3077733a5e47a
sen <64001680+dahyen0o@users.noreply.github.com>
2023-08-02 01:21:28 +0900

    chore: stop.sh 작성

---
+++ script/stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/floney-dev-deploy"
+JAR="$ROOT_PATH/application.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 Not Found" >> $STOP_LOG
+else
+  echo "서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi
