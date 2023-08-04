7f21520aca232cca5de8f687f171b4c59f4a25fa
sen <64001680+dahyen0o@users.noreply.github.com>
2023-08-04 16:36:17 +0900

    chore: stop.sh 업데이트

--- script/stop.sh
+++ script/stop.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/floney-dev-deploy"
-JAR="$ROOT_PATH/floney-dev-build.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
+ROOT_PATH="/home/ubuntu/Floney-Server"
+JAR="$ROOT_PATH/Floney-0.0.1-SNAPSHOT.jar"
+
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
-if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 Not Found" >> $STOP_LOG
+TIME_NOW=$(date +%c)
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+if [ -z $CURRENT_PID ]; then
+  echo "[ $TIME_NOW ] Running application not found." >> $DEPLOY_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
-  kill "$SERVICE_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+  echo "$[ TIME_NOW ] Terminate application PID : $CURRENT_PID" >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
 fi
