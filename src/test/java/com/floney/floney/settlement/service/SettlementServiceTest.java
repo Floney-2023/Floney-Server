9a3443788cf5c89b0ee336d7260aeeb2699154df
sen <seeunchoi99@gmail.com>
2023-08-05 14:06:56 +0900

    #117 test: SettlementServiceTest의 유저 목록에 없는 지출 유저로 정산 내역 생성 실패 테스트

--- src/test/java/com/floney/floney/settlement/service/SettlementServiceTest.java
+++ src/test/java/com/floney/floney/settlement/service/SettlementServiceTest.java
@@ -1,12 +1,14 @@
 package com.floney.floney.settlement.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.floney.floney.book.entity.Book;
 import com.floney.floney.book.repository.BookRepository;
+import com.floney.floney.common.exception.settlement.OutcomeUserNotFoundException;
 import com.floney.floney.settlement.dto.request.SettlementRequest;
 import com.floney.floney.settlement.dto.response.SettlementResponse;
 import com.floney.floney.user.dto.constant.Provider;
@@ -141,5 +143,47 @@ class SettlementServiceTest {
         assertThat(response.getTotalOutcome()).isEqualTo(30000);
         assertThat(response.getOutcome()).isEqualTo(10000);
         assertThat(response.getDetails()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("정산 내역을 생성하는데 실패한다 - 유저 목록에 없는 지출 내역의 유저")
+    void createSettlement_fail_OutcomeUserNotFoundException() throws JsonProcessingException {
+        // given
+        final String json = "{\"bookKey\":\"abcdefgh\"," +
+                "\"startDate\":\"2000-01-01\"," +
+                "\"endDate\":\"2000-01-02\"," +
+                "\"userEmails\":[\"test01@email.com\",\"test02@email.com\"]," +
+                "\"outcomes\":[{" +
+                "\"outcome\":10000," +
+                "\"userEmail\":\"test@email.com\"}]}";
+
+        final SettlementRequest request = new ObjectMapper().registerModule(new JavaTimeModule())
+                .readValue(json, SettlementRequest.class);
+
+        final Book book = Book.builder()
+                .bookKey(request.getBookKey())
+                .code("code")
+                .name("name")
+                .build();
+
+        bookRepository.save(book);
+
+        final User user1 = User.builder()
+                .email("test01@email.com")
+                .nickname("nickname")
+                .provider(Provider.EMAIL)
+                .build();
+        final User user2 = User.builder()
+                .email("test02@email.com")
+                .nickname("nickname")
+                .provider(Provider.EMAIL)
+                .build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        // when & then
+        assertThatThrownBy(() -> settlementService.create(request))
+                .isInstanceOf(OutcomeUserNotFoundException.class);
     }
 }
