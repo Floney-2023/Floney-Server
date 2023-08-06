bc7ab4a50facd66c617389dc95f94a644136f89e
sienna <20200803@sungshin.ac.kr>
2023-08-06 23:08:14 +0900

    #113 chore : 주석 추가

--- src/main/java/com/floney/floney/analyze/controller/AnalyzeController.java
+++ src/main/java/com/floney/floney/analyze/controller/AnalyzeController.java
@@ -21,16 +21,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AnalyzeController {
     private final AnalyzeService analyzeService;
 
+    /**
+     * 
+     * @body AnalyzeByCategoryRequest 카테고리 별 분석 요청
+     * @return AnalyzeResponse 해당 카테고리 분석 결과
+     */
     @GetMapping("/category")
     public ResponseEntity<?> analyzeByCategory(@RequestBody AnalyzeByCategoryRequest request) {
         return new ResponseEntity<>(analyzeService.analyzeByCategory(request), HttpStatus.OK);
     }
 
+    /**
+     * 
+     * @body AnalyzeRequestByBudget 예산 분석 요청
+     * @return AnalyzeResponseByBudget 예산 분석 응답
+     */
     @PostMapping("/budget")
     public ResponseEntity<?> analyzeByBudget(@RequestBody AnalyzeRequestByBudget request) {
         return new ResponseEntity<>(analyzeService.analyzeByBudget(request), HttpStatus.OK);
     }
 
+    /**
+     * 
+     * @body AnalyzeRequestByAsset 자산 분석 요청
+     * @return AnalyzeResponseByAsset 자산 분석 응답
+     */
     @PostMapping("/asset")
     public ResponseEntity<?> analyzeByAsset(@RequestBody AnalyzeRequestByAsset request) {
         return new ResponseEntity<>(analyzeService.analyzeByAsset(request), HttpStatus.OK);
