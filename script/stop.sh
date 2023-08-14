c21a46ed05dc6bd7542dae5d1e36c9fc210ef6bc
sen <64001680+dahyen0o@users.noreply.github.com>
2023-08-15 02:07:14 +0900

    fix: stop.sh 변수 오류 수정

--- script/stop.sh
+++ script/stop.sh
@@ -3,17 +3,16 @@
 ROOT_PATH="/home/ubuntu/Floney-Server"
 JAR_FILE="$ROOT_PATH/build/libs/Floney-0.0.1-SNAPSHOT.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+DEPLOY_LOG="$ROOT_PATH/deploy.log"
 
 SERVICE_PID=$(pgrep -f $JAR_FILE) # 실행중인 Spring 서버의 PID
 
 TIME_NOW=$(date +%c)
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-if [ -z $CURRENT_PID ]; then
+if [ -z $SERVICE_PID ]; then
   echo "[ $TIME_NOW ] Running application not found." >> $DEPLOY_LOG
 else
-  echo "[ $TIME_NOW ] Terminate application PID : $CURRENT_PID" >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  echo "[ $TIME_NOW ] Terminate application PID : $SERVICE_PID" >> $DEPLOY_LOG
+  kill -15 $SERVICE_PID
+  sleep 5
 fi
