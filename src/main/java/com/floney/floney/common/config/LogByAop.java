688f9e58a999b2a1676d86f19b4502662174be09
sienna <20200803@sungshin.ac.kr>
2023-08-17 22:56:13 +0900

    #132 feat : aop를 사용한 메서드 호출 로그

---
+++ src/main/java/com/floney/floney/common/config/LogByAop.java
@@ -0,0 +1,24 @@
+package com.floney.floney.common.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogByAop {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    @Pointcut("execution(* com.floney.floney.*..*.*(..))")
+    private void cutAllRequest() {
+    }
+
+    @Before("cutAllRequest()")
+    public void beforeRequest(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        logger.info("METHOD : " + className + "." + methodName);
+    }
+}
