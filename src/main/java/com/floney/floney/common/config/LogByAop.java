49e48424138c7a5b84b9a3df5af389c8bd0cc6e5
sienna <20200803@sungshin.ac.kr>
2023-08-22 18:22:26 +0900

    #134 chore : AOP 로깅 데이터 수정

--- src/main/java/com/floney/floney/common/config/LogByAop.java
+++ src/main/java/com/floney/floney/common/config/LogByAop.java
@@ -6,21 +6,36 @@ import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Aspect
 @Component
 public class LogByAop {
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-    @Pointcut("execution(* com.floney.floney.*..*.*(..))")
+    @Pointcut("execution(* com.floney.floney.*.controller..*.*(..))")
     private void cutAllRequest() {
     }
 
     @Before("cutAllRequest()")
     public void beforeRequest(JoinPoint joinPoint) {
-        String methodName = joinPoint.getSignature().getName();
-        String className = joinPoint.getTarget().getClass().getSimpleName();
-        logger.info("METHOD : " + className + "." + methodName);
+        HttpServletRequest request =
+            ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String method = request.getMethod();
+        String path = request.getRequestURI();
+
+        List<String> data = Arrays.stream(joinPoint.getArgs())
+            .map(Object::toString)
+            .toList();
+
+        logger.info("[Access Log] " + method + " " + path + " [Data] " + data);
     }
 
 }
