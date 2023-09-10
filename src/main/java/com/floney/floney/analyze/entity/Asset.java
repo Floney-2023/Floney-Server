0de6c8f3dd00f6713c3f4510e0893d1ad8d496f5
sienna <20200803@sungshin.ac.kr>
2023-09-10 19:17:10 +0900

    #165 refactor : 월별 예산 변경

---
+++ src/main/java/com/floney/floney/analyze/entity/Asset.java
@@ -0,0 +1,57 @@
+package com.floney.floney.analyze.entity;
+
+import com.floney.floney.book.dto.request.UpdateAssetRequest;
+import com.floney.floney.book.entity.Book;
+import com.floney.floney.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Asset extends BaseEntity {
+
+    private LocalDate date;
+
+    private Long money;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Book book;
+
+    @Builder
+    private Asset(LocalDate date, Long money, Book book) {
+        this.date = date;
+        this.money = money;
+        this.book = book;
+    }
+
+    public static Asset of(Book book, UpdateAssetRequest request) {
+        return Asset.builder()
+            .money(request.getAsset())
+            .date(request.getDate())
+            .book(book)
+            .build();
+    }
+
+    public static Asset init() {
+        return Asset.builder()
+            .money(0L)
+            .build();
+    }
+
+
+    public void update(UpdateAssetRequest request) {
+        this.money = request.getAsset();
+    }
+}
