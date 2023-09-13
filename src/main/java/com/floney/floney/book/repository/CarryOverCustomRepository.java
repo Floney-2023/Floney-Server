d7b75d2d38770efa2c859312a6b08af2470af8ce
sienna <20200803@sungshin.ac.kr>
2023-09-13 21:48:37 +0900

    #174 fix : merge conflict 해결

---
+++ src/main/java/com/floney/floney/book/repository/CarryOverCustomRepository.java
@@ -0,0 +1,10 @@
+package com.floney.floney.book.repository;
+
+import com.floney.floney.book.entity.CarryOver;
+
+import java.util.List;
+
+public interface CarryOverCustomRepository {
+
+    void deleteAllCarryOver(String bookKey);
+}
