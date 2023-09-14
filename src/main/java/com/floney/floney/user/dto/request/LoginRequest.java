ccf39b1c475366f614ee598d87543025815b94f9
sen <seeunchoi99@gmail.com>
2023-09-14 22:05:12 +0900

    #184 refactor: UserServiceTest에 테스트 추가

--- src/main/java/com/floney/floney/user/dto/request/LoginRequest.java
+++ src/main/java/com/floney/floney/user/dto/request/LoginRequest.java
@@ -1,14 +1,16 @@
 package com.floney.floney.user.dto.request;
 
 import javax.validation.constraints.NotNull;
-
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@EqualsAndHashCode
 public class LoginRequest {
 
     @NotNull private String email;
