fbbb95c205c71e1e91f4a6e119270dca1183f695
sen <seeunchoi99@gmail.com>
2023-10-16 13:09:04 +0900

    #208 fix: Category 아이디 생성 방식 Identity로 수정

--- src/main/java/com/floney/floney/book/entity/Category.java
+++ src/main/java/com/floney/floney/book/entity/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.JoinColumn;
@@ -28,7 +29,7 @@ import org.hibernate.annotations.OnDeleteAction;
 public abstract class Category {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
