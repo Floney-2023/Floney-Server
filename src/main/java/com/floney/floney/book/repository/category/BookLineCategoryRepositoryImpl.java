5efbdab919229db4003730c0685386415fe9b95e
sen <seeunchoi99@gmail.com>
2023-10-18 23:33:26 +0900

    #208 feat: BookLineCategoryCustomRepository에 BookLineCategory 삭제하는 메서드 추가

--- src/main/java/com/floney/floney/book/repository/category/BookLineCategoryRepositoryImpl.java
+++ src/main/java/com/floney/floney/book/repository/category/BookLineCategoryRepositoryImpl.java
@@ -1,42 +1,73 @@
 package com.floney.floney.book.repository.category;
 
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-
-import static com.floney.floney.book.entity.QBook.book;
-import static com.floney.floney.book.entity.QBookLine.bookLine;
 import static com.floney.floney.book.entity.QBookLineCategory.bookLineCategory;
+import static com.floney.floney.common.constant.Status.ACTIVE;
 import static com.floney.floney.common.constant.Status.INACTIVE;
 
+import com.floney.floney.book.entity.Book;
+import com.floney.floney.book.entity.BookUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 @Repository
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookLineCategoryRepositoryImpl implements BookLineCategoryCustomRepository {
+
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public void deleteBookLineCategory(String bookKey) {
+    @Transactional
+    public void inactiveAllByBookKey(final String bookKey) {
         jpaQueryFactory.update(bookLineCategory)
-            .set(bookLineCategory.status, INACTIVE)
-            .set(bookLineCategory.updatedAt, LocalDateTime.now())
-            .where(bookLineCategory.bookLine.id.in(
-                JPAExpressions.select(bookLine.id)
-                    .from(bookLine)
-                    .leftJoin(bookLine.book, book)
-                    .where(book.bookKey.eq(bookKey))
-            ))
-            .execute();
+                .set(bookLineCategory.status, INACTIVE)
+                .set(bookLineCategory.updatedAt, LocalDateTime.now())
+                .where(
+                        bookLineCategory.bookLine.book.bookKey.eq(bookKey),
+                        bookLineCategory.status.eq(ACTIVE)
+                )
+                .execute();
     }
 
     @Override
-    public void deleteBookLineCategoryById(Long id) {
+    @Transactional
+    public void inactiveAllByBookLineId(final Long bookLineId) {
         jpaQueryFactory.update(bookLineCategory)
-            .set(bookLineCategory.status, INACTIVE)
-            .set(bookLineCategory.updatedAt, LocalDateTime.now())
-            .where(bookLineCategory.bookLine.id.eq(id))
-            .execute();
+                .set(bookLineCategory.status, INACTIVE)
+                .set(bookLineCategory.updatedAt, LocalDateTime.now())
+                .where(
+                        bookLineCategory.bookLine.id.eq(bookLineId),
+                        bookLineCategory.status.eq(ACTIVE)
+                )
+                .execute();
+    }
+
+    @Override
+    @Transactional
+    public void inactiveAllByBookUser(final BookUser bookUser) {
+        jpaQueryFactory.update(bookLineCategory)
+                .set(bookLineCategory.status, INACTIVE)
+                .set(bookLineCategory.updatedAt, LocalDateTime.now())
+                .where(
+                        bookLineCategory.bookLine.writer.eq(bookUser),
+                        bookLineCategory.status.eq(ACTIVE)
+                )
+                .execute();
+    }
+
+    @Override
+    @Transactional
+    public void inactiveAllByBook(final Book book) {
+        jpaQueryFactory.update(bookLineCategory)
+                .set(bookLineCategory.status, INACTIVE)
+                .set(bookLineCategory.updatedAt, LocalDateTime.now())
+                .where(
+                        bookLineCategory.bookLine.book.eq(book),
+                        bookLineCategory.status.eq(ACTIVE)
+                )
+                .execute();
     }
 }
