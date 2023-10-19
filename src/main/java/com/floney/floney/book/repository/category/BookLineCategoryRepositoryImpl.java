1471339d32ebef914c4d0502af36f8626a9d7b02
sen <seeunchoi99@gmail.com>
2023-10-20 02:31:27 +0900

    #208 fix: BookLineCategoryRepositoryImpl에서 cross join이 발생하는 쿼리 수정

--- src/main/java/com/floney/floney/book/repository/category/BookLineCategoryRepositoryImpl.java
+++ src/main/java/com/floney/floney/book/repository/category/BookLineCategoryRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.floney.floney.book.repository.category;
 
+import static com.floney.floney.book.entity.QBook.book;
+import static com.floney.floney.book.entity.QBookLine.bookLine;
 import static com.floney.floney.book.entity.QBookLineCategory.bookLineCategory;
+import static com.floney.floney.book.entity.QBookUser.bookUser;
 import static com.floney.floney.common.constant.Status.ACTIVE;
 import static com.floney.floney.common.constant.Status.INACTIVE;
 
 import com.floney.floney.book.entity.Book;
 import com.floney.floney.book.entity.BookUser;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +27,16 @@ public class BookLineCategoryRepositoryImpl implements BookLineCategoryCustomRep
     @Override
     @Transactional
     public void inactiveAllByBookKey(final String bookKey) {
+        final JPQLQuery<Long> bookLineByBookKey = JPAExpressions.select(bookLine.id)
+                .from(bookLine)
+                .leftJoin(bookLine.book, book)
+                .where(book.bookKey.eq(bookKey));
+
         jpaQueryFactory.update(bookLineCategory)
                 .set(bookLineCategory.status, INACTIVE)
                 .set(bookLineCategory.updatedAt, LocalDateTime.now())
                 .where(
-                        bookLineCategory.bookLine.book.bookKey.eq(bookKey),
+                        bookLineCategory.bookLine.id.in(bookLineByBookKey),
                         bookLineCategory.status.eq(ACTIVE)
                 )
                 .execute();
@@ -47,12 +57,17 @@ public class BookLineCategoryRepositoryImpl implements BookLineCategoryCustomRep
 
     @Override
     @Transactional
-    public void inactiveAllByBookUser(final BookUser bookUser) {
+    public void inactiveAllByBookUser(final BookUser targetBookUser) {
+        final JPQLQuery<Long> bookLineByBookUser = JPAExpressions.select(bookLine.id)
+                .from(bookLine)
+                .innerJoin(bookLine.writer, bookUser)
+                .where(bookUser.eq(targetBookUser));
+
         jpaQueryFactory.update(bookLineCategory)
                 .set(bookLineCategory.status, INACTIVE)
                 .set(bookLineCategory.updatedAt, LocalDateTime.now())
                 .where(
-                        bookLineCategory.bookLine.writer.eq(bookUser),
+                        bookLineCategory.bookLine.id.in(bookLineByBookUser),
                         bookLineCategory.status.eq(ACTIVE)
                 )
                 .execute();
@@ -60,12 +75,17 @@ public class BookLineCategoryRepositoryImpl implements BookLineCategoryCustomRep
 
     @Override
     @Transactional
-    public void inactiveAllByBook(final Book book) {
+    public void inactiveAllByBook(final Book targetBook) {
+        final JPQLQuery<Long> bookLineByBook = JPAExpressions.select(bookLine.id)
+                .from(bookLine)
+                .leftJoin(bookLine.book, book)
+                .where(book.eq(targetBook));
+
         jpaQueryFactory.update(bookLineCategory)
                 .set(bookLineCategory.status, INACTIVE)
                 .set(bookLineCategory.updatedAt, LocalDateTime.now())
                 .where(
-                        bookLineCategory.bookLine.book.eq(book),
+                        bookLineCategory.bookLine.id.in(bookLineByBook),
                         bookLineCategory.status.eq(ACTIVE)
                 )
                 .execute();
