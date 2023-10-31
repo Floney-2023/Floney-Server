aea6145e2061d6c31a0839ad6f46196ae3924ecd
sen <seeunchoi99@gmail.com>
2023-10-31 13:19:32 +0900

    #215 fix: SettlementCustomRepositoryImpl.inactiveAllByBookKey() 쿼리에서 cross join이 발생하지 않도록 수정

--- src/main/java/com/floney/floney/settlement/repository/SettlementCustomRepositoryImpl.java
+++ src/main/java/com/floney/floney/settlement/repository/SettlementCustomRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.floney.floney.settlement.repository;
 
 
+import com.floney.floney.book.entity.Book;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -8,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import static com.floney.floney.book.entity.QBook.book;
 import static com.floney.floney.common.constant.Status.ACTIVE;
 import static com.floney.floney.common.constant.Status.INACTIVE;
 import static com.floney.floney.settlement.domain.entity.QSettlement.settlement;
@@ -22,11 +26,14 @@ public class SettlementCustomRepositoryImpl implements SettlementCustomRepositor
     @Override
     @Transactional
     public void inactiveAllByBookKey(final String bookKey) {
+        final JPQLQuery<Book> bookByBookKey = JPAExpressions.selectFrom(book)
+                .where(book.bookKey.eq(bookKey));
+
         jpaQueryFactory.update(settlement)
                 .set(settlement.status, INACTIVE)
                 .set(settlement.updatedAt, LocalDateTime.now())
                 .where(
-                        settlement.book.bookKey.eq(bookKey),
+                        settlement.book.eq(bookByBookKey),
                         settlement.status.eq(ACTIVE)
                 )
                 .execute();
