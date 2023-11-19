a1f77cf39502622b66571e9e325a8ec2fdf0a07e
sienna <20200803@sungshin.ac.kr>
2023-11-19 19:47:28 +0900

    #260 fix : 구독 관련 test 코드 삭제

--- src/test/java/com/floney/floney/book/BookServiceTest.java
+++ src/test/java/com/floney/floney/book/BookServiceTest.java
@@ -64,61 +64,6 @@ public class BookServiceTest {
     }
 
     @Test
-    @DisplayName("가계부 생성시 구독을 안했다면, 참여한 가계부가 1 초과일 시 가계부를 만들 수 없다")
-    void default_book_create_exception() {
-        given(bookUserRepository.countBookUserByUserAndStatus(any(User.class), any(ACTIVE.getClass())))
-                .willReturn(2);
-
-        assertThatThrownBy(() -> bookService.addBook(UserFixture.createUser(), createBookRequest()))
-                .isInstanceOf(NotSubscribeException.class);
-    }
-
-    @Test
-    @DisplayName("가계부 생성시 구독을 안했다면, 참여한 가계부가 1미만 일 시 가계부를 만든다")
-    void default_book_create() {
-        given(bookUserRepository.countBookUserByUserAndStatus(any(User.class), any(ACTIVE.getClass())))
-                .willReturn(0);
-        given(bookRepository.save(any(Book.class)))
-                .willReturn(BookFixture.createBook());
-
-        Assertions.assertThat(bookService.addBook(UserFixture.createUser(), createBookRequest()).getClass())
-                .isEqualTo(CreateBookResponse.class);
-    }
-
-    @Test
-    @DisplayName("가계부 참여 시 구독을 안했다면, 유저가 현재 참여한 가계부가 2 초과일시, 구독 제한 예외가 터진다")
-    void default_book_join_exception() {
-        given(bookUserRepository.countBookUserByUserAndStatus(any(User.class), any(ACTIVE.getClass())))
-                .willReturn(3);
-
-        given(bookRepository.findBookExclusivelyByCodeAndStatus(any(String.class), any(Status.class)))
-                .willReturn(Optional.ofNullable(createBook()));
-
-        CustomUserDetails customUserDetails = CustomUserDetails.of(UserFixture.createUser());
-
-        assertThatThrownBy(() -> bookService.joinWithCode(customUserDetails, codeJoinRequest()))
-                .isInstanceOf(NotSubscribeException.class);
-    }
-
-    @Test
-    @DisplayName("가계부 참여 시 참여 하려는 가계부의 정원 초과 시 가계부 정원 초과 예외 발생")
-    void default_book_join() {
-        given(bookUserRepository.countBookUserByUserAndStatus(any(User.class), any(ACTIVE.getClass())))
-                .willReturn(0);
-
-        // 가계부 정원이 2인 가계부 생성
-        given(bookRepository.findBookExclusivelyByCodeAndStatus(any(String.class), any(Status.class)))
-                .willReturn(Optional.ofNullable(createBook()));
-
-        given(bookUserRepository.countByBookExclusively(any(Book.class))).willReturn(4);
-
-        CustomUserDetails customUserDetails = CustomUserDetails.of(UserFixture.createUser());
-
-        assertThatThrownBy(() -> bookService.joinWithCode(customUserDetails, codeJoinRequest()))
-                .isInstanceOf(MaxMemberException.class);
-    }
-
-    @Test
     @DisplayName("가계부 이름 변경을 요청한다")
     void change_name() {
         String changeTo = "newName";
