372f4e37ca1b1e037da948745e3e1edc52843126
sen <seeunchoi99@gmail.com>
2023-12-12 19:51:31 +0900

    #319 chore: 기본값을 default로 설정하고 (book_id, date) 복합 인덱스가 추가된 ASSET 테이블 DDL 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `asset`
+(
+    `id`         bigint       NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(6)  NOT NULL DEFAULT now(),
+    `status`     varchar(255) NOT NULL DEFAULT 'ACTIVE',
+    `updated_at` datetime(6)  NOT NULL DEFAULT now(),
+    `date`       date         NOT NULL,
+    `money`      double       NOT NULL,
+    `book_id`    bigint       NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `book_and_date_on_asset` (`book_id`, `date`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
