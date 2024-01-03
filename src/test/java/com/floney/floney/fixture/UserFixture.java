25af755a3bd13bd9011a7f0d1371e82cd029b24d
sen <seeunchoi99@gmail.com>
2024-01-03 18:44:38 +0900

    #357 test: UserFixture 내 User.profileImg 를 기본 값으로 변경

--- src/test/java/com/floney/floney/fixture/UserFixture.java
+++ src/test/java/com/floney/floney/fixture/UserFixture.java
@@ -1,11 +1,10 @@
 package com.floney.floney.fixture;
 
+import static com.floney.floney.fixture.BookFixture.EMAIL;
+
 import com.floney.floney.user.dto.constant.Provider;
 import com.floney.floney.user.entity.User;
-
 import java.time.LocalDateTime;
-
-import static com.floney.floney.fixture.BookFixture.EMAIL;
 
 public class UserFixture {
 
@@ -14,8 +13,20 @@ public class UserFixture {
                 .nickname("floney")
                 .email(EMAIL)
                 .password("password")
-                .profileImg("img")
+                .profileImg("user_default")
                 .lastAdTime(LocalDateTime.of(2023, 4, 15, 12, 30, 45))
+                .provider(Provider.EMAIL)
+                .receiveMarketing(true)
+                .build();
+    }
+
+    public static User emailUserWithEmail(final String email) {
+        return User.builder()
+                .nickname("floney")
+                .email(email)
+                .password("password")
+                .profileImg("user_default")
+                .lastAdTime(LocalDateTime.now())
                 .provider(Provider.EMAIL)
                 .receiveMarketing(true)
                 .build();
@@ -26,7 +37,7 @@ public class UserFixture {
                 .nickname("floney")
                 .email("floney2@naver.com")
                 .password("password")
-                .profileImg("img")
+                .profileImg("user_default")
                 .lastAdTime(LocalDateTime.of(2023, 4, 15, 12, 30, 45))
                 .provider(Provider.KAKAO)
                 .receiveMarketing(false)
