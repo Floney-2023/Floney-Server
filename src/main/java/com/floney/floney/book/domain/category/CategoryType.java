24c25034a73de35f5f6770ae876be4019ff9d774
sen <seeunchoi99@gmail.com>
2024-01-24 20:18:37 +0900

    #362 feat: CategoryType 에서 LineType 과 아닌 Type 명시, meaning 으로 CategoryType 을 찾는 메서드 구현

--- src/main/java/com/floney/floney/book/domain/category/CategoryType.java
+++ src/main/java/com/floney/floney/book/domain/category/CategoryType.java
@@ -2,17 +2,37 @@ package com.floney.floney.book.domain.category;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum CategoryType {
 
+    /* Line Type */
     INCOME("수입"),
     OUTCOME("지출"),
     TRANSFER("이체"),
+
+    /* Others */
     ASSET("자산");
 
     private final String meaning;
 
     CategoryType(final String meaning) {
         this.meaning = meaning;
+    }
+
+    public static CategoryType findByMeaning(final String meaning) {
+        return Arrays.stream(CategoryType.values())
+            .filter(categoryType -> categoryType.meaning.equals(meaning))
+            .findFirst()
+            .orElseThrow();
+    }
+
+    public static CategoryType findLineByMeaning(final String meaning) {
+        return Arrays.stream(CategoryType.values())
+            .filter(categoryType -> categoryType.meaning.equals(meaning))
+            .filter(categoryType -> !CategoryType.ASSET.equals(categoryType))
+            .findFirst()
+            .orElseThrow();
     }
 }
