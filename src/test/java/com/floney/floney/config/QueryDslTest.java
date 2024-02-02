254e96022d81988c58dbf7f40efb0a7e18250d0c
sienna <20200803@sungshin.ac.kr>
2024-02-02 11:43:42 +0900

    #362 fix : annotation retention 추가

--- src/test/java/com/floney/floney/config/QueryDslTest.java
+++ src/test/java/com/floney/floney/config/QueryDslTest.java
@@ -4,8 +4,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @Import(QueryFactoryConfig.class)
 @DataJpaTest
+@Retention(value = RetentionPolicy.RUNTIME)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public @interface QueryDslTest {
 }
