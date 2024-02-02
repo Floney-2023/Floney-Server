b687dbf726b28eeef6e1ed7cd9afde69fa5a0ca8
sen <seeunchoi99@gmail.com>
2024-02-02 15:10:19 +0900

    #362 fix: 테스트 디비 설정 오류 수정

--- src/test/java/com/floney/floney/config/QueryDslTest.java
+++ src/test/java/com/floney/floney/config/QueryDslTest.java
@@ -4,6 +4,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 @Import(QueryFactoryConfig.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
