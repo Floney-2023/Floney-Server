db55f1a1e04b2b655768f1af7bdaecfd8e6c8ed5
sienna <20200803@sungshin.ac.kr>
2024-03-02 14:02:55 +0900

    #392 test : CarryOverTest 추가

---
+++ src/test/java/com/floney/floney/book/CarryOverServiceTest.java
@@ -0,0 +1,110 @@
+package com.floney.floney.book;
+
+import com.floney.floney.analyze.service.CarryOverServiceImpl;
+import com.floney.floney.book.domain.entity.Book;
+import com.floney.floney.book.domain.entity.BookLine;
+import com.floney.floney.book.domain.entity.BookLineCategory;
+import com.floney.floney.book.domain.entity.BookUser;
+import com.floney.floney.book.repository.analyze.CarryOverRepository;
+import com.floney.floney.fixture.*;
+import com.floney.floney.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@DisplayName("단위 테스트 : CarryOver")
+@ExtendWith(MockitoExtension.class)
+public class CarryOverServiceTest {
+
+    @InjectMocks
+    private CarryOverServiceImpl carryOverService;
+
+    @Mock
+    private CarryOverRepository carryOverRepository;
+
+    @Nested
+    @DisplayName("createCarryOver()를 실행할 때")
+    class Describe_CreateCarryOver {
+        User user;
+        Book book;
+        String bookKey;
+        BookUser bookUser;
+
+        BookLine bookLine;
+        String assetSubCategoryName = "자산";
+
+        @BeforeEach()
+        void init() {
+            user = UserFixture.emailUser();
+            book = BookFixture.createBook();
+            bookKey = book.getBookKey();
+            bookUser = BookUserFixture.createBookUser(book, user);
+        }
+
+        @Nested
+        @DisplayName("카테고리가 수입인 가계부 내역이 주어지면")
+        class Context_With_IncomeBookLine {
+            @BeforeEach
+            void init() {
+                String incomeSubCategoryName = "급여";
+                final BookLineCategory bookLineCategory = BookLineCategoryFixture.incomeBookLineCategory(book, incomeSubCategoryName, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+            }
+
+            @Test
+            @DisplayName("이월 내역이 생성 된다")
+            void it_create_carryOver() {
+                carryOverService.createCarryOver(bookLine);
+                verify(carryOverRepository, times(1)).saveAll(any(List.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("카테고리가 지출인 가계부 내역이 주어지면")
+        class Context_With_OutcomeBookLine {
+
+            @BeforeEach
+            void init() {
+                String outcomeSubCategoryName = "식비";
+                final BookLineCategory bookLineCategory = BookLineCategoryFixture.outcomeBookLineCategory(book, outcomeSubCategoryName, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+            }
+
+            @Test
+            @DisplayName("이월 내역이 생성 된다")
+            void it_create_carryOver() {
+                carryOverService.createCarryOver(bookLine);
+                verify(carryOverRepository, times(1)).saveAll(any(List.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("카테고리가 이체인 가계부 내역이 주어지면")
+        class Context_With_TransferBookLine {
+            @BeforeEach
+            void init() {
+                String transferSubCategoryName = "이체";
+                final BookLineCategory bookLineCategory = BookLineCategoryFixture.transferBookLineCategory(book, transferSubCategoryName, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+            }
+
+            @Test
+            @DisplayName("이월 내역이 생성 되지 않는다")
+            void it_didnt_create_carryOver() {
+                carryOverService.createCarryOver(bookLine);
+                verify(carryOverRepository, never()).saveAll(any(List.class));
+            }
+        }
+    }
+}
--- src/test/java/com/floney/floney/book/CarryOverTest.java
+++ src/test/java/com/floney/floney/book/CarryOverTest.java
@@ -1,6 +1,7 @@
 package com.floney.floney.book;
 
 
+import com.floney.floney.book.domain.category.CategoryType;
 import com.floney.floney.book.domain.entity.*;
 import com.floney.floney.fixture.*;
 import com.floney.floney.user.entity.User;
@@ -22,7 +23,6 @@ public class CarryOverTest {
         Book book;
         String bookKey;
         BookUser bookUser;
-
         BookLine bookLine;
         String assetSubCategoryName = "자산";
 
@@ -51,7 +51,6 @@ public class CarryOverTest {
                 CarryOver carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
                 Assertions.assertThat(carryOver.getMoney()).isEqualTo(bookLine.getMoney());
             }
-
         }
 
         @Nested
@@ -72,6 +71,159 @@ public class CarryOverTest {
                 Assertions.assertThat(carryOver.getMoney()).isEqualTo(-1 * bookLine.getMoney());
             }
 
+        }
+    }
+
+    @Nested
+    @DisplayName("update()를 실행할 때")
+    class Describe_Update {
+        User user;
+        Book book;
+        String bookKey;
+        BookUser bookUser;
+        BookLine bookLine;
+        String assetSubCategoryName = "자산";
+
+        @BeforeEach
+        void init() {
+            user = UserFixture.emailUser();
+            book = BookFixture.createBook();
+            bookKey = book.getBookKey();
+            bookUser = BookUserFixture.createBookUser(book, user);
+        }
+
+        @Nested
+        @DisplayName("수입 내역이 주어지면")
+        class Context_With_IncomeMoney {
+
+            CarryOver carryOver;
+
+            @BeforeEach
+            void init() {
+                String incomeSubCategoryName = "급여";
+                final BookLineCategory bookLineCategory = BookLineCategoryFixture.incomeBookLineCategory(book, incomeSubCategoryName, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+                carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
+            }
+
+            @Test
+            @DisplayName("이월 금액이 증가한다")
+            void it_increase_money() {
+                carryOver.update(1000, CategoryType.INCOME);
+                Assertions.assertThat(carryOver.getMoney()).isGreaterThan(bookLine.getMoney());
+            }
+        }
+
+        @Nested
+        @DisplayName("지출 내역이 주어지면")
+        class Context_With_OutcomeMoney {
+
+            CarryOver carryOver;
+
+            @BeforeEach
+            void init() {
+                String outcomeSubCategory = "식비";
+                final BookLineCategory bookLineCategory = BookLineCategoryFixture.outcomeBookLineCategory(book, outcomeSubCategory, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+                carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
+            }
+
+            @Test
+            @DisplayName("이월 금액이 감소한다")
+            void it_decrease_money() {
+                carryOver.update(1000, CategoryType.OUTCOME);
+                Assertions.assertThat(carryOver.getMoney()).isLessThan(bookLine.getMoney());
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("delete()를 실행할 때")
+    class Describe_Delete {
+        User user;
+        Book book;
+        String bookKey;
+        BookUser bookUser;
+
+        BookLine bookLine;
+        String assetSubCategoryName = "자산";
+
+        @BeforeEach
+        void init() {
+            user = UserFixture.emailUser();
+            book = BookFixture.createBook();
+            bookKey = book.getBookKey();
+            bookUser = BookUserFixture.createBookUser(book, user);
+        }
+
+        @Nested
+        @DisplayName("수입 내역이 주어지면")
+        class Context_With_IncomeMoney {
+
+            CarryOver carryOver;
+
+            BookLineCategory bookLineCategory;
+
+            @BeforeEach
+            void init() {
+                String incomeSubCategoryName = "급여";
+                bookLineCategory = BookLineCategoryFixture.incomeBookLineCategory(book, incomeSubCategoryName, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+                carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
+            }
+
+            @Test
+            @DisplayName("이월 금액이 감소한다")
+            void it_decrease_money() {
+                carryOver.delete(1000, bookLineCategory);
+                Assertions.assertThat(carryOver.getMoney()).isLessThan(bookLine.getMoney());
+            }
+        }
+
+        @Nested
+        @DisplayName("지출 내역이 주어지면")
+        class Context_With_OutcomeMoney {
+
+            CarryOver carryOver;
+            BookLineCategory bookLineCategory;
+
+            @BeforeEach
+            void init() {
+                String outcomeSubCategory = "식비";
+                bookLineCategory = BookLineCategoryFixture.outcomeBookLineCategory(book, outcomeSubCategory, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+                carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
+            }
+
+            @Test
+            @DisplayName("이월 금액이 증가한다")
+            void it_decrease_money() {
+                carryOver.delete(1000, bookLineCategory);
+                Assertions.assertThat(carryOver.getMoney()).isGreaterThan(-1 * bookLine.getMoney());
+            }
+        }
+
+        @Nested
+        @DisplayName("이체 내역이 주어지면")
+        class Context_With_TransferMoney {
+
+            CarryOver carryOver;
+            BookLineCategory bookLineCategory;
+
+            @BeforeEach
+            void init() {
+                String transferSubCategory = "이체";
+                bookLineCategory = BookLineCategoryFixture.transferBookLineCategory(book, transferSubCategory, assetSubCategoryName);
+                bookLine = BookLineFixture.createWithDate(book, bookUser, bookLineCategory, LocalDate.now());
+                carryOver = CarryOver.of(bookLine, LocalDate.now().plusDays(1));
+            }
+
+            @Test
+            @DisplayName("이월 금액은 변하지 않는다")
+            void it_maintain_money() {
+                carryOver.delete(1000, bookLineCategory);
+                Assertions.assertThat(carryOver.getMoney()).isEqualTo(bookLine.getMoney());
+            }
         }
     }
 }
