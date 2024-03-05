c999167d5ee6ebe08f9893fcb406b84b9e5e31dd
sienna <20200803@sungshin.ac.kr>
2024-03-06 08:36:48 +0900

    #401 fix : 반복 내역 종료 조건 수정

--- src/test/java/com/floney/floney/book/repository/BookLineCustomRepositoryTest.java
+++ src/test/java/com/floney/floney/book/repository/BookLineCustomRepositoryTest.java
@@ -2,6 +2,7 @@ package com.floney.floney.book.repository;
 
 import com.floney.floney.analyze.dto.request.AnalyzeByCategoryRequest;
 import com.floney.floney.analyze.dto.response.AnalyzeResponseByCategory;
+import com.floney.floney.book.domain.RepeatDuration;
 import com.floney.floney.book.domain.category.CategoryType;
 import com.floney.floney.book.domain.category.entity.Category;
 import com.floney.floney.book.domain.category.entity.Subcategory;
@@ -181,8 +182,8 @@ class BookLineCustomRepositoryTest {
             @Test
             @DisplayName("BookLineWithWriterView 목록을 반환한다.")
             void it_returns_bookLineWithWriterViews() {
-                assertThat(bookLineRepository.allLinesByDay(date, bookKey))
-                    .hasSize(2);
+                assertThat(bookLineRepository.allLinesByDay(date, bookKey).get(0).getRepeatDuration())
+                    .isEqualTo(RepeatDuration.NONE);
             }
         }
 
