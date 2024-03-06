5a91af152a6a3551eb652fd04d0a997f72bccb61
sienna <20200803@sungshin.ac.kr>
2024-03-06 21:33:06 +0900

    #407 fix : 이월 설정 upsert 로 동시성 처리

--- src/main/java/com/floney/floney/book/repository/analyze/CarryOverRepository.java
+++ src/main/java/com/floney/floney/book/repository/analyze/CarryOverRepository.java
@@ -4,11 +4,21 @@ import com.floney.floney.book.domain.entity.Book;
 import com.floney.floney.book.domain.entity.CarryOver;
 import com.floney.floney.common.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface CarryOverRepository extends JpaRepository<CarryOver, Long>, CarryOverCustomRepository {
     Optional<CarryOver> findCarryOverByDateAndBookAndStatus(LocalDate targetDate, Book book, Status status);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+        value = "insert into carry_over (date, money, book_id) values (:date, :money, :book) " +
+            "on duplicate key update money = money + :money, updated_at = now()",
+        nativeQuery = true
+    )
+    void upsertMoneyByDateAndBook(LocalDate date, Book book, double money);
 
 }
---
+++ src/main/resources/db/migration/mysql/V12__add_unique_key_in_carry_over.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX `book_and_date_on_carry_over` ON `carry_over` (`book_id`, `date`, `status`);
+
