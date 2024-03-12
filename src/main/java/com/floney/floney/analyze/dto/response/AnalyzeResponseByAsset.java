69b53d7512b32833f6d75e07e31df21a2f0eab5e
sen <seeunchoi99@gmail.com>
2024-03-12 22:53:11 +0900

    #427 refactor: 생성자 롬복으로 변경

--- src/main/java/com/floney/floney/analyze/dto/response/AnalyzeResponseByAsset.java
+++ src/main/java/com/floney/floney/analyze/dto/response/AnalyzeResponseByAsset.java
@@ -1,26 +1,23 @@
 package com.floney.floney.analyze.dto.response;
 
 import com.floney.floney.book.dto.process.AssetInfo;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AnalyzeResponseByAsset {
+
     private final double difference;
     private final double initAsset;
     private final double currentAsset;
     private final Map<LocalDate, AssetInfo> assetInfo;
 
-    private AnalyzeResponseByAsset(double difference, double initAsset, double currentAsset, Map<LocalDate, AssetInfo> assetInfo) {
-        this.difference = difference;
-        this.currentAsset = currentAsset;
-        this.initAsset = initAsset;
-        this.assetInfo = assetInfo;
-    }
-
-    public static AnalyzeResponseByAsset of(double difference, double initAsset, double currentAsset, Map<LocalDate,AssetInfo> assetInfo) {
+    public static AnalyzeResponseByAsset of(double difference, double initAsset, double currentAsset, Map<LocalDate, AssetInfo> assetInfo) {
         return new AnalyzeResponseByAsset(difference, initAsset, currentAsset, assetInfo);
     }
 }
