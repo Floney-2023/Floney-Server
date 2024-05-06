12e8ad1f676f15136fe15e8848583000863d6fe2
sen <seeunchoi99@gmail.com>
2024-05-06 15:26:10 +0900

    #462 test: 즐겨찾기 단건 조회 API 인수 테스트

--- src/test/java/com/floney/floney/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/com/floney/floney/acceptance/FavoriteAcceptanceTest.java
@@ -149,4 +149,61 @@ public class FavoriteAcceptanceTest {
             }
         }
     }
+
+    @Nested
+    @DisplayName("즐겨찾기를 조회할 때")
+    class Describe_GetFavorite {
+
+        @Nested
+        @DisplayName("존재하는 즐겨찾기의 id로 요청한 경우")
+        class Context_With_FavoriteIdExists {
+
+            int favoriteId;
+            String accessToken;
+            String bookKey;
+
+            @BeforeEach
+            void init() {
+                accessToken = UserApiFixture.loginAfterSignup(UserFixture.emailUser()).getAccessToken();
+                bookKey = BookApiFixture.createBook(accessToken).getBookKey();
+                favoriteId = RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .auth().oauth2(accessToken)
+                    .body("""
+                        {
+                            "money": 1000,
+                            "lineCategoryName": "지출",
+                            "lineSubcategoryName": "식비",
+                            "assetSubcategoryName": "현금"
+                        }
+                        """)
+                    .when()
+                    .post("/books/{key}/favorites", bookKey)
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("id", notNullValue())
+                    .extract().path("id");
+            }
+
+            @Test
+            @DisplayName("즐겨찾기 정보를 반환한다.")
+            void it_returns_favoriteInfo() {
+                RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .auth().oauth2(accessToken)
+                    .when()
+                    .get("/books/{key}/favorites/{id}", bookKey, favoriteId)
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .body(
+                        "id", is(favoriteId),
+                        "money", is(1000.0f),
+                        "description", nullValue(),
+                        "lineCategoryName", is("지출"),
+                        "lineSubcategoryName", is("식비"),
+                        "assetSubcategoryName", is("현금")
+                    );
+            }
+        }
+    }
 }
