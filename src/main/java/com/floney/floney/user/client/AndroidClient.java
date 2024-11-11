e3cbc5ac52b8e0c685fd1a48ef6927fefd358095
sienna011022 <20200803@sungshin.ac.kr>
2024-11-11 20:27:58 +0900

    feat : 오타수정

--- src/main/java/com/floney/floney/user/client/AndroidClient.java
+++ src/main/java/com/floney/floney/user/client/AndroidClient.java
@@ -87,7 +87,7 @@ public class AndroidClient {
 
         // 결과 로그 출력
         logger.info("Decoded DTO: " + dto);
-        logger.info("subscription" + dto.getSubscriptionNotification())
+        logger.info("subscription" + dto.getSubscriptionNotification());
         logger.info(dto.getSubscriptionNotification().getPurchaseToken());
     }
 
